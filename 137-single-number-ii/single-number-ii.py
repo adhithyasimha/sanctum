fecde64539765e49ccfd12efe02a097ca231d532
Adhithya <69459749+adhithyasimha@users.noreply.github.com>
2025-06-16 19:47:08 +0530

    Time: 3 ms (69.84%) | Memory: 19.7 MB (7.97%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count_map={}
+        for num in nums:
+            if num in count_map:
+                count_map[num]+=1
+            else:
+                count_map[num]=1
+            
+        for num,count in count_map.items():
+            if count==1:
+                return num
+        
